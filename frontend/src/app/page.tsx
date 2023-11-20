e24b9943892680386b0a53437f8c0e94bf841342
fnmatheus <nasc.matheusfrancisco@gmail.com>
2023-11-20 07:50:52 -0300

    feat: login base structure

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,7 +3,19 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Flex Motos</h1>
+      <div></div>
+      <div>
+        <h1>FlexMotos</h1>
+        <div>
+          <form className="text-black" action="">
+            <input type="text" name="" id="" />
+            <input type="text" name="" id="" />
+            <input type="password" name="" id="" />
+            <button className="bg-white">Entrar</button>
+          </form>
+        </div>
+      </div>
+      <div></div>
     </main>
   )
 }
