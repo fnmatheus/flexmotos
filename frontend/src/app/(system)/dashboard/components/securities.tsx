691d9a7c318a11968ea7d20db9c838748a6006f9
fnmatheus <nasc.matheusfrancisco@gmail.com>
2023-11-28 23:04:48 -0300

    feat: securities model

--- frontend/src/app/(system)/dashboard/components/securities.tsx
+++ frontend/src/app/(system)/dashboard/components/securities.tsx
@@ -2,7 +2,24 @@ import { IProps } from '../../utils/interfaces';
 
 const Securities: React.FC<IProps> = ({token}: IProps) => {
   return (
-    <div>Securities!</div>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Cauções</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className="flex gap-2">
+              <p>Nome do cliente</p>
+              <p>R$ 9999999,99</p>
+              <button>Devolver</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 }
 
