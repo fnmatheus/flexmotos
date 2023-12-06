8192559c717a1777523dbe77e3ef2f5f3adbe1fb
fnmatheus <nasc.matheusfrancisco@gmail.com>
2023-12-06 01:00:28 -0300

    feat: start dashboardTable

--- frontend/src/app/(system)/dashboard/components/dashboardTable.tsx
+++ frontend/src/app/(system)/dashboard/components/dashboardTable.tsx
@@ -1,44 +1,47 @@
 import React from 'react';
 import Popup from '../../../components/popup';
 import { IDashboardTable } from '../../utils/interfaces';
+import { dashboardTable } from '@/app/utils/classnames';
 
 const DashboardTable: React.FC<IDashboardTable> = (
   {tableTitle, vehicles, hasButton, buttonText, handleButton, hasThirdText, hasPopup, popup, popuptext, handleYes, handleNo}: IDashboardTable
 ) => {
   return (
-    <div>
-      <table>
-        <thead>
-          <tr>
-            <th>{tableTitle}</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            vehicles.map((vehicle) => {
-              const [plate, model] = vehicle;
-              return (
-                <tr key={plate}>
-                  <td className="flex gap-2">
-                    <p>{model}</p>
-                    <p>{plate}</p>
-                    {
-                      hasButton && handleButton &&
-                      <button onClick={() => handleButton(vehicle)}>
-                        {buttonText}
-                      </button>
-                    }
-                    {
-                      hasThirdText &&
-                      <p>{vehicle[2]}</p>
-                    }
-                  </td>
-                </tr>
-              );
-            })
-          }
-        </tbody>
-      </table>
+    <div className="w-full">
+      <div className={dashboardTable}>
+        <table className="flex flex-col w-full text-xl">
+          <thead className="border-b border-zinc-400 w-full flex justify-center">
+            <tr>
+              <th className="font-normal">{tableTitle}</th>
+            </tr>
+          </thead>
+          <tbody className="flex flex-col w-full m-1">
+            {
+              vehicles.map((vehicle) => {
+                const [plate, model] = vehicle;
+                return (
+                  <tr className="flex w-full items-center" key={plate}>
+                    <td className="border-spacing-0 flex w-full justify-between px-2">
+                      <p className="w-2/5">{model}</p>
+                      <p className="text-center w-1/5">{plate}</p>
+                      {
+                        hasButton && handleButton &&
+                        <button className="w-1/6 text-center bg-amber-400 font-semibold text-black rounded-md" onClick={() => handleButton(vehicle)}>
+                          {buttonText}
+                        </button>
+                      }
+                      {
+                        hasThirdText &&
+                        <p className="w-1/5">{vehicle[2]}</p>
+                      }
+                    </td>
+                  </tr>
+                );
+              })
+            }
+          </tbody>
+        </table>
+      </div>
       {
         hasPopup && popup && popup.length > 0 &&
         <Popup
