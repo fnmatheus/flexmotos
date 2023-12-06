ee16f96ac72844a137f184a57f54185583df46ca
fnmatheus <nasc.matheusfrancisco@gmail.com>
2023-12-05 22:45:28 -0300

    feat: navbar

--- frontend/src/app/components/svgs/index.tsx
+++ frontend/src/app/components/svgs/index.tsx
@@ -1,2 +1,7 @@
 export {default as Flexmotos} from './flexmotos.svg';
 export {default as Fnmatheus} from './fnmatheus.svg';
+export {default as Logout} from './logout.svg';
+export {default as Users} from './users.svg';
+export {default as Dashboard} from './dashboard.svg';
+export {default as Clients} from './clients.svg';
+export {default as Vehicles} from './vehicles.svg';
